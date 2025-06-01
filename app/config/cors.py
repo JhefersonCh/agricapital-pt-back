80e0dcfbe5e7d3d2923077f1cf0d3af190085927
Jheff <divegas43@gmail.com>
2025-06-01 11:55:33 -0500

    :bug: Fix: backend url in cors

--- app/config/cors.py
+++ app/config/cors.py
@@ -7,11 +7,7 @@ load_dotenv()
 
 
 def setup_cors(app: FastAPI):
-    if os.getenv("ENVIRONMENT") == "development":
-        origins = [os.getenv("BACKEND_URL", "http://localhost:8000")]
-    else:
-        origins = [os.getenv("FRONTEND_URL").rstrip("/")]
-
+    origins = [os.getenv("FRONTEND_URL").rstrip("/")]
     methods = os.getenv("ALLOWED_METHODS", "GET,POST,PATCH,DELETE,OPTIONS").split(",")
     headers = os.getenv("ALLOWED_HEADERS", "Content-Type,Authorization").split(",")
 
